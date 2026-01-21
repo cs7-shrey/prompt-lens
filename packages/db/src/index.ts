81ed2e804a189b124ac265db7eadc240582d0974
Shrey <sshrey0007@gmail.com>
2026-01-21 13:06:05 +0530

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,17 @@
+import { neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { env } from "@prompt-lens/env/server";
+import ws from "ws";
+
+import { PrismaClient } from "../prisma/generated/client";
+
+neonConfig.webSocketConstructor = ws;
+neonConfig.poolQueryViaFetch = true;
+
+const adapter = new PrismaNeon({
+  connectionString: env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
